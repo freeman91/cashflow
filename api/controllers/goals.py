8762974f0a9acd962cd843cbf831c7f2b511485f
Addison Freeman <addisonfreeman91@gmail.com>
2021-06-17 23:08:25 -0400

    init controller files

---
+++ api/controllers/goals.py
@@ -0,0 +1,3 @@
+def update(month: int, year: int, goals: list):
+    """only can update before the month starts"""
+    pass
---
+++ api/controllers/networths.py
@@ -0,0 +1,8 @@
+def compile_networth():
+    """
+    - execute last day of the month at 23:59
+    - find current value of all assets, amount of debts
+    - create net worth item
+    - update networth on user item
+    """
+    pass
