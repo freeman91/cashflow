47197e74a68edb247737264c6c0fa8337c915249
Addison Freeman <addisonfreeman91@gmail.com>
2022-04-06 22:04:06 -0400

    finish restore script

--- mongo-restore.sh
+++ mongo-restore.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/env zsh
 
+# Drop and restore database from a backup
+
 source .env
 
-docker cp ${<backup dir>} mongodb:/data
+filename="cashflow-backup-$(date +"%Y-%m-%d")"
 
-docker exec -i mongodb mongorestore /data/${backup dir}
+docker cp ~/mongo-backups/$BACKUP_DB_DIR mongodb:/data
+
+docker exec -i mongodb /usr/bin/mongorestore --port 27017 --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --db database /$filename/database --drop
 
 exit
