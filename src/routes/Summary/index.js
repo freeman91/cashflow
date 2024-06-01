37c7e90b02c12d72e70eaabd0810c549211ce266
Addison Freeman <addisonfreeman91@gmail.com>
2024-06-01 12:03:07 -0400

    wip

--- src/routes/Summary/index.js
+++ src/routes/Summary/index.js
@@ -1,24 +1,69 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import dayjs from 'dayjs';
+import get from 'lodash/get';
 
 import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+
+import { setAppBar } from '../../store/appSettings';
 
 export default function Summary() {
-  const [date] = useState(dayjs());
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const [year, setYear] = useState(null);
+  const [month, setMonth] = useState(null);
+
+  useEffect(() => {
+    const today = dayjs();
+    const _year = get(location.pathname.split('/'), '2', today.year());
+    const _month = get(location.pathname.split('/'), '3', null);
+    setYear(_year);
+    setMonth(_month);
+  }, [location]);
+
+  useEffect(() => {
+    dispatch(
+      setAppBar({
+        title: 'summary',
+        // leftAction: null,
+        // rightAction: null,
+      })
+    );
+  }, [dispatch]);
+
+  console.log('month: ', month);
+
   return (
-    <>
-      <Stack spacing={2} sx={{ width: '100%' }}>
+    <Grid
+      container
+      spacing={1}
+      sx={{
+        pl: 1,
+        pr: 1,
+        pt: 1,
+        mb: 8,
+      }}
+    >
+      <Grid item xs={12}>
         <Typography variant='body1' align='center' fontWeight='bold'>
-          {date.format('YYYY')}
+          {year}
         </Typography>
+      </Grid>
+
+      <Grid item xs={12}>
         <Typography variant='body1' align='center'>
           Totals
         </Typography>
+      </Grid>
+
+      <Grid item xs={12}>
         <Typography variant='body1' align='center'>
           List of Months
         </Typography>
-      </Stack>
-    </>
+      </Grid>
+    </Grid>
   );
 }
