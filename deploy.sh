f24d4c7334000ffabacf02fde6feb3a712f06a49
Addison Freeman <addisonfreeman91@gmail.com>
2025-09-03 12:27:57 -0400

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 docker-compose down
 git pull origin $(git branch --show-current)
 echo "Pulled $(git log --oneline | wc -l) line changes"
-docker-compose up -d --build
+docker compose up -d --build
 
 echo "Deployed"
